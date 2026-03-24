0668283376f182d0ba3b067cb798cba1b8de54a0
mariocofre <mariocofre2023@gmail.com>
2026-03-24 00:02:11 -0300

    update

--- frontend/patiperro-project/src/app/routes.tsx
+++ frontend/patiperro-project/src/app/routes.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../features/auth/pages/Login/Login";
 import Register from "../features/auth/pages/Register/Register";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to={"/Login"}/>} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
